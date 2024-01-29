ce94577081eb88531eb348f1b8e288481ab20dfa
Erfan Navab <erfannavab@gmail.com>
2024-01-29 11:43:49 +0330

    added data for clerk

--- src/test/java/nvb/dev/officemanagement/MotherObject.java
+++ src/test/java/nvb/dev/officemanagement/MotherObject.java
@@ -1,4 +1,31 @@
 package nvb.dev.officemanagement;
 
+import nvb.dev.officemanagement.model.entity.ClerkEntity;
+
 public class MotherObject {
+
+    public static final String ANY_STRING = "dummy";
+    public static final Integer ANY_NUMBER = 20;
+
+    public static final String ANY_UPDATED_STRING = "updatedDummy";
+    public static final Integer ANY_UPDATED_NUMBER = 25;
+
+    public static ClerkEntity anyValidClerk() {
+        return ClerkEntity.builder()
+                .firstName(ANY_STRING)
+                .lastName(ANY_STRING)
+                .department(ANY_STRING)
+                .age(ANY_NUMBER)
+                .build();
+    }
+
+    public static ClerkEntity anyValidUpdatedClerk() {
+        return ClerkEntity.builder()
+                .firstName(ANY_UPDATED_STRING)
+                .lastName(ANY_UPDATED_STRING)
+                .department(ANY_UPDATED_STRING)
+                .age(ANY_UPDATED_NUMBER)
+                .build();
+    }
+
 }
