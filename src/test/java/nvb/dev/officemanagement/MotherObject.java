7ac29c091cd89e561b2607d8d6d0cd044fa2c6c1
Erfan Navab <erfannavab@gmail.com>
2024-01-29 16:05:32 +0330

    - added new methods - modified some of the methods

--- src/test/java/nvb/dev/officemanagement/MotherObject.java
+++ src/test/java/nvb/dev/officemanagement/MotherObject.java
@@ -1,6 +1,8 @@
 package nvb.dev.officemanagement;
 
+import nvb.dev.officemanagement.model.Address;
 import nvb.dev.officemanagement.model.dto.ClerkDto;
+import nvb.dev.officemanagement.model.dto.DocumentDto;
 import nvb.dev.officemanagement.model.entity.ClerkEntity;
 import nvb.dev.officemanagement.model.entity.DocumentEntity;
 import nvb.dev.officemanagement.model.entity.ManagerEntity;
@@ -9,6 +11,8 @@ import nvb.dev.officemanagement.model.entity.OfficeEntity;
 import java.util.Set;
 
 public class MotherObject {
+
+    public static final Long ANY_ID = 1L;
 
     public static final String ANY_STRING = "dummy";
     public static final Integer ANY_NUMBER = 20;
@@ -22,26 +26,28 @@ public class MotherObject {
                 .lastName(ANY_STRING)
                 .department(ANY_STRING)
                 .age(ANY_NUMBER)
-                .office(anyValidOffice())
-                .manager(anyValidManager())
-                .documents(Set.of(anyValidDocument()))
+                .office(new OfficeEntity())
+                .manager(new ManagerEntity())
+                .documents(Set.of(new DocumentEntity()))
                 .build();
     }
 
     public static ClerkDto anyValidClerkDto() {
         return ClerkDto.builder()
+                .id(ANY_ID)
                 .firstName(ANY_STRING)
                 .lastName(ANY_STRING)
                 .department(ANY_STRING)
                 .age(ANY_NUMBER)
-                .office(anyValidOffice())
-                .manager(anyValidManager())
-                .documents(Set.of(anyValidDocument()))
+                .office(new OfficeEntity())
+                .manager(new ManagerEntity())
+                .documents(Set.of(new DocumentEntity()))
                 .build();
     }
 
     public static ClerkEntity anyValidUpdatedClerk() {
         return ClerkEntity.builder()
+                .id(ANY_ID)
                 .firstName(ANY_UPDATED_STRING)
                 .lastName(ANY_UPDATED_STRING)
                 .department(ANY_UPDATED_STRING)
@@ -51,13 +57,29 @@ public class MotherObject {
 
     public static DocumentEntity anyValidDocument() {
         return DocumentEntity.builder()
+                .id(ANY_ID)
                 .title(ANY_STRING)
                 .description(ANY_STRING)
+                .managers(Set.of(new ManagerEntity()))
+                .clerks(Set.of(new ClerkEntity()))
+                .office(new OfficeEntity())
+                .build();
+    }
+
+    public static DocumentDto anyValidDocumentDto() {
+        return DocumentDto.builder()
+                .id(ANY_ID)
+                .title(ANY_STRING)
+                .description(ANY_STRING)
+                .managers(Set.of(new ManagerEntity()))
+                .clerks(Set.of(new ClerkEntity()))
+                .office(new OfficeEntity())
                 .build();
     }
 
     public static DocumentEntity anyValidUpdatedDocument() {
         return DocumentEntity.builder()
+                .id(ANY_ID)
                 .title(ANY_UPDATED_STRING)
                 .description(ANY_UPDATED_STRING)
                 .build();
@@ -65,15 +87,20 @@ public class MotherObject {
 
     public static ManagerEntity anyValidManager() {
         return ManagerEntity.builder()
+                .id(ANY_ID)
                 .firstName(ANY_STRING)
                 .lastName(ANY_STRING)
                 .department(ANY_STRING)
                 .age(ANY_NUMBER)
+                .office(new OfficeEntity())
+                .clerks(Set.of(new ClerkEntity()))
+                .documents(Set.of(new DocumentEntity()))
                 .build();
     }
 
     public static ManagerEntity anyValidUpdatedManager() {
         return ManagerEntity.builder()
+                .id(ANY_ID)
                 .firstName(ANY_UPDATED_STRING)
                 .lastName(ANY_UPDATED_STRING)
                 .department(ANY_UPDATED_STRING)
@@ -83,17 +110,30 @@ public class MotherObject {
 
     public static OfficeEntity anyValidOffice() {
         return OfficeEntity.builder()
+                .id(ANY_ID)
                 .officeName(ANY_STRING)
                 .officeCode(ANY_STRING)
                 .officePhoneNumber(ANY_STRING)
+                .address(anyValidAddress())
+                .managers(Set.of(new ManagerEntity()))
+                .clerks(Set.of(new ClerkEntity()))
+                .documents(Set.of(new DocumentEntity()))
                 .build();
     }
 
     public static OfficeEntity anyValidUpdatedOffice() {
         return OfficeEntity.builder()
+                .id(ANY_ID)
                 .officeName(ANY_UPDATED_STRING)
                 .officeCode(ANY_UPDATED_STRING)
                 .officePhoneNumber(ANY_UPDATED_STRING)
+                .build();
+    }
+
+    public static Address anyValidAddress() {
+        return Address.builder()
+                .city(ANY_STRING)
+                .country(ANY_STRING)
                 .build();
     }
 
