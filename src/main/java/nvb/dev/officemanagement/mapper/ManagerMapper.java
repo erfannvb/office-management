700d2ee8abace50054869078afede43ea86c5f0b
Erfan Navab <erfannavab@gmail.com>
2024-01-29 16:16:31 +0330

    implemented manager mapper

---
+++ src/main/java/nvb/dev/officemanagement/mapper/ManagerMapper.java
@@ -0,0 +1,32 @@
+package nvb.dev.officemanagement.mapper;
+
+import nvb.dev.officemanagement.model.dto.ManagerDto;
+import nvb.dev.officemanagement.model.entity.ManagerEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ManagerMapper {
+
+    @Mapping(source = "managerEntity.id", target = "id")
+    @Mapping(source = "managerEntity.firstName", target = "firstName")
+    @Mapping(source = "managerEntity.lastName", target = "lastName")
+    @Mapping(source = "managerEntity.department", target = "department")
+    @Mapping(source = "managerEntity.age", target = "age")
+    @Mapping(source = "managerEntity.office", target = "office")
+    @Mapping(source = "managerEntity.clerks", target = "clerks")
+    @Mapping(source = "managerEntity.documents", target = "documents")
+    ManagerDto toManagerDto(ManagerEntity managerEntity);
+
+    @Mapping(source = "managerDto.id", target = "id")
+    @Mapping(source = "managerDto.firstName", target = "firstName")
+    @Mapping(source = "managerDto.lastName", target = "lastName")
+    @Mapping(source = "managerDto.department", target = "department")
+    @Mapping(source = "managerDto.age", target = "age")
+    @Mapping(source = "managerDto.office", target = "office")
+    @Mapping(source = "managerDto.clerks", target = "clerks")
+    @Mapping(source = "managerDto.documents", target = "documents")
+    ManagerEntity toManagerEntity(ManagerDto managerDto);
+
+}
