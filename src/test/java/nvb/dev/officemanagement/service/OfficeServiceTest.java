15f9174e388e561bfda6a8f8efaa733e61b0014c
Erfan Navab <erfannavab@gmail.com>
2024-01-30 23:13:57 +0330

    completed the tests with 100% coverage

--- src/test/java/nvb/dev/officemanagement/service/OfficeServiceTest.java
+++ src/test/java/nvb/dev/officemanagement/service/OfficeServiceTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static nvb.dev.officemanagement.MotherObject.anyValidOffice;
+import static nvb.dev.officemanagement.MotherObject.anyValidUpdatedOffice;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -96,6 +97,116 @@ class OfficeServiceTest {
 
         assertThrows(NoDataFoundException.class, () -> officeService.getOfficeByOfficeName(""));
         verify(officeRepository, atLeastOnce()).findByOfficeName(anyString());
+    }
+
+    @Test
+    void testThatUpdateOfficeUpdatesTheExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.of(anyValidOffice()));
+        when(officeRepository.save(any(OfficeEntity.class))).thenReturn(anyValidUpdatedOffice());
+
+        OfficeEntity updatedOffice = officeService.updateOffice(1L, anyValidUpdatedOffice());
+
+        assertEquals("updatedDummy", updatedOffice.getOfficeName());
+        assertEquals("updatedDummy", updatedOffice.getOfficeCode());
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, atLeastOnce()).save(any(OfficeEntity.class));
+    }
+
+    @Test
+    void testThatUpdateOfficeCannotUpdateTheNonExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () ->
+                officeService.updateOffice(1L, anyValidUpdatedOffice()));
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, never()).save(any(OfficeEntity.class));
+    }
+
+    @Test
+    void testThatIsExistsReturnsTrue() {
+        when(officeRepository.existsById(anyLong())).thenReturn(true);
+
+        boolean exists = officeService.isExists(1L);
+
+        assertTrue(exists);
+        verify(officeRepository, atLeastOnce()).existsById(anyLong());
+    }
+
+    @Test
+    void testThatIsExistsReturnsFalse() {
+        when(officeRepository.existsById(anyLong())).thenReturn(false);
+
+        boolean exists = officeService.isExists(1L);
+
+        assertFalse(exists);
+        verify(officeRepository, atLeastOnce()).existsById(anyLong());
+    }
+
+    @Test
+    void testThatPartialUpdateUpdatesTheExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.of(anyValidOffice()));
+        when(officeRepository.save(any(OfficeEntity.class))).thenReturn(anyValidUpdatedOffice());
+
+        OfficeEntity partialUpdate = officeService.partialUpdate(1L, anyValidUpdatedOffice());
+
+        assertEquals("updatedDummy", partialUpdate.getOfficeName());
+        assertEquals("updatedDummy", partialUpdate.getOfficeCode());
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, atLeastOnce()).save(any(OfficeEntity.class));
+    }
+
+    @Test
+    void testThatPartialUpdateCannotUpdateTheNonExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () ->
+                officeService.partialUpdate(1L, anyValidUpdatedOffice()));
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, never()).save(any(OfficeEntity.class));
+    }
+
+    @Test
+    void testThatDeleteOfficeDeletesTheExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.of(anyValidOffice()));
+
+        officeService.deleteOffice(1L);
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, atLeastOnce()).deleteById(anyLong());
+    }
+
+    @Test
+    void testThatDeleteOfficeCannotDeleteTheNonExistingOffice() {
+        when(officeRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> officeService.deleteOffice(1L));
+
+        verify(officeRepository, atLeastOnce()).findById(anyLong());
+        verify(officeRepository, never()).deleteById(anyLong());
+    }
+
+    @Test
+    void testThatDeleteAllOfficesDeletesTheExistingOffices() {
+        when(officeRepository.findAll()).thenReturn(List.of(anyValidOffice(), anyValidOffice()));
+
+        officeService.deleteAllOffices();
+
+        verify(officeRepository, atLeastOnce()).findAll();
+        verify(officeRepository, atLeastOnce()).deleteAll();
+    }
+
+    @Test
+    void testThatDeleteAllOfficesCannotDeleteTheNonExistingOffices() {
+        when(officeRepository.findAll()).thenReturn(List.of());
+
+        assertThrows(NoDataFoundException.class, () -> officeService.deleteAllOffices());
+
+        verify(officeRepository, atLeastOnce()).findAll();
+        verify(officeRepository, never()).deleteAll();
     }
 
 }
