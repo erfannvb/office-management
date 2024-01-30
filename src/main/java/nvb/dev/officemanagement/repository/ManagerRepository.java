a73504bd42e23af7bc5fc4eae949eb5d89718248
Erfan Navab <erfannavab@gmail.com>
2024-01-31 03:18:02 +0330

    added new methods

--- src/main/java/nvb/dev/officemanagement/repository/ManagerRepository.java
+++ src/main/java/nvb/dev/officemanagement/repository/ManagerRepository.java
@@ -3,7 +3,19 @@ package nvb.dev.officemanagement.repository;
 import nvb.dev.officemanagement.model.entity.ManagerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ManagerRepository extends JpaRepository<ManagerEntity, Long> {
+
+    Optional<ManagerEntity> findByOfficeId(long officeId);
+
+    List<ManagerEntity> findAllByOfficeId(long officeId);
+
+    @Transactional
+    void deleteByOfficeId(long officeId);
+
 }
