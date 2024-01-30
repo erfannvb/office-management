892fb9e3d4afefd7c072a442945a874c9b25880f
Erfan Navab <erfannavab@gmail.com>
2024-01-30 20:04:58 +0330

    added two methods

--- src/main/java/nvb/dev/officemanagement/repository/OfficeRepository.java
+++ src/main/java/nvb/dev/officemanagement/repository/OfficeRepository.java
@@ -4,6 +4,13 @@ import nvb.dev.officemanagement.model.entity.OfficeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OfficeRepository extends JpaRepository<OfficeEntity, Long> {
+
+    Optional<OfficeEntity> findByOfficeName(String officeName);
+
+    Optional<OfficeEntity> findByOfficeCode(String officeCode);
+
 }
