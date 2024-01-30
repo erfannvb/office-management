6b817f4ef08d69cb053fbe9efc15bfa1b0d2cadd
Erfan Navab <erfannavab@gmail.com>
2024-01-30 11:06:20 +0330

    added new methods

--- src/main/java/nvb/dev/officemanagement/repository/ClerkRepository.java
+++ src/main/java/nvb/dev/officemanagement/repository/ClerkRepository.java
@@ -3,7 +3,18 @@ package nvb.dev.officemanagement.repository;
 import nvb.dev.officemanagement.model.entity.ClerkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClerkRepository extends JpaRepository<ClerkEntity, Long> {
+
+    Optional<ClerkEntity> findByOfficeIdAndManagerId(long officeId, long managerId);
+
+    List<ClerkEntity> findByOfficeId(long officeId);
+
+    List<ClerkEntity> findByManagerId(long managerId);
+
 }
