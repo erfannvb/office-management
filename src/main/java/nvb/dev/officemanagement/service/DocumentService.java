79dd09cc0999130f98208f5ee3a6fdfcf9d2263c
Erfan Navab <erfannavab@gmail.com>
2024-02-01 10:37:56 +0330

    implemented document service

--- src/main/java/nvb/dev/officemanagement/service/DocumentService.java
+++ src/main/java/nvb/dev/officemanagement/service/DocumentService.java
@@ -1,7 +1,26 @@
 package nvb.dev.officemanagement.service;
 
+import nvb.dev.officemanagement.model.entity.DocumentEntity;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface DocumentService {
 
+    DocumentEntity createDocument(long officeId, DocumentEntity document);
 
+    Optional<DocumentEntity> getDocumentById(long documentId);
+
+    List<DocumentEntity> getAllDocumentsByOfficeId(long officeId);
+
+    DocumentEntity updateDocument(long documentId, DocumentEntity document);
+
+    DocumentEntity partialUpdate(long documentId, DocumentEntity document);
+
+    void deleteDocument(long documentId);
+
+    void deleteAllDocumentsOfOffice(long officeId);
+
+    boolean isExists(long documentId);
 
 }
--- src/main/java/nvb/dev/officemanagement/service/impl/DocumentServiceImpl.java
+++ src/main/java/nvb/dev/officemanagement/service/impl/DocumentServiceImpl.java
@@ -1,12 +1,100 @@
 package nvb.dev.officemanagement.service.impl;
 
 import lombok.AllArgsConstructor;
+import nvb.dev.officemanagement.exception.EntityNotFoundException;
+import nvb.dev.officemanagement.model.entity.DocumentEntity;
+import nvb.dev.officemanagement.model.entity.OfficeEntity;
+import nvb.dev.officemanagement.repository.DocumentRepository;
+import nvb.dev.officemanagement.repository.OfficeRepository;
 import nvb.dev.officemanagement.service.DocumentService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class DocumentServiceImpl implements DocumentService {
 
+    private final DocumentRepository documentRepository;
+    private final OfficeRepository officeRepository;
 
+    @Override
+    public DocumentEntity createDocument(long officeId, DocumentEntity document) {
+        OfficeEntity officeEntity = unwrapOffice(officeRepository.findById(officeId), officeId);
+        document.setOffice(officeEntity);
+        return documentRepository.save(document);
+    }
+
+    @Override
+    public Optional<DocumentEntity> getDocumentById(long documentId) {
+        return Optional.ofNullable(documentRepository.findById(documentId)
+                .orElseThrow(() -> new EntityNotFoundException(DocumentEntity.class, documentId)));
+    }
+
+    @Override
+    public List<DocumentEntity> getAllDocumentsByOfficeId(long officeId) {
+        return documentRepository.findAllByOfficeId(officeId);
+    }
+
+    @Override
+    public DocumentEntity updateDocument(long documentId, DocumentEntity document) {
+        Optional<DocumentEntity> optionalDocument = documentRepository.findById(documentId);
+        if (optionalDocument.isPresent()) {
+
+            DocumentEntity currentDocument = optionalDocument.get();
+
+            currentDocument.setTitle(document.getTitle());
+            currentDocument.setDescription(document.getDescription());
+
+            return documentRepository.save(currentDocument);
+
+        } else {
+            throw new EntityNotFoundException(DocumentEntity.class, documentId);
+        }
+    }
+
+    @Override
+    public DocumentEntity partialUpdate(long documentId, DocumentEntity document) {
+        document.setId(documentId);
+
+        return documentRepository.findById(documentId).map(existingDocument -> {
+
+            Optional.ofNullable(document.getTitle()).ifPresent(existingDocument::setTitle);
+            Optional.ofNullable(document.getDescription()).ifPresent(existingDocument::setDescription);
+
+            return documentRepository.save(existingDocument);
+
+        }).orElseThrow(() -> new EntityNotFoundException(DocumentEntity.class, documentId));
+    }
+
+    @Override
+    public void deleteDocument(long documentId) {
+        Optional<DocumentEntity> optionalDocument = documentRepository.findById(documentId);
+        if (optionalDocument.isPresent()) {
+            documentRepository.deleteById(documentId);
+        } else {
+            throw new EntityNotFoundException(DocumentEntity.class, documentId);
+        }
+    }
+
+    @Override
+    public void deleteAllDocumentsOfOffice(long officeId) {
+        Optional<OfficeEntity> optionalOffice = officeRepository.findById(officeId);
+        if (optionalOffice.isPresent()) {
+            documentRepository.deleteByOfficeId(officeId);
+        } else {
+            throw new EntityNotFoundException(OfficeEntity.class, officeId);
+        }
+    }
+
+    @Override
+    public boolean isExists(long documentId) {
+        return false;
+    }
+
+    private static OfficeEntity unwrapOffice(Optional<OfficeEntity> entity, long officeId) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(OfficeEntity.class, officeId);
+    }
 }
