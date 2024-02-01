31d14925bf75fb54b6976606e509b67c90176dea
Erfan Navab <erfannavab@gmail.com>
2024-02-01 10:37:22 +0330

    added two methods

--- src/main/java/nvb/dev/officemanagement/repository/DocumentRepository.java
+++ src/main/java/nvb/dev/officemanagement/repository/DocumentRepository.java
@@ -3,7 +3,16 @@ package nvb.dev.officemanagement.repository;
 import nvb.dev.officemanagement.model.entity.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
+
+    List<DocumentEntity> findAllByOfficeId(long officeId);
+
+    @Transactional
+    void deleteByOfficeId(long officeId);
+
 }
