0eaf9f7662991380f7d4acc12a1f5683ddfa8669
Erfan Navab <erfannavab@gmail.com>
2024-02-01 13:03:47 +0330

    added role to the mappings

--- src/main/java/nvb/dev/officemanagement/mapper/UserMapper.java
+++ src/main/java/nvb/dev/officemanagement/mapper/UserMapper.java
@@ -11,10 +11,12 @@ public interface UserMapper {
 
     @Mapping(source = "userEntity.username", target = "username")
     @Mapping(source = "userEntity.password", target = "password")
+    @Mapping(source = "userEntity.role", target = "role")
     UserDto toUserDto(UserEntity userEntity);
 
     @Mapping(source = "userDto.username", target = "username")
     @Mapping(source = "userDto.password", target = "password")
+    @Mapping(source = "userDto.role", target = "role")
     UserEntity toUserEntity(UserDto userDto);
 
 }
