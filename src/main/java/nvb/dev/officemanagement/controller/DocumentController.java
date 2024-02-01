712ffc18e7908ffddedd367a2c6788c38c3dbf41
Erfan Navab <erfannavab@gmail.com>
2024-02-01 17:18:20 +0330

    changed request mapping path

--- src/main/java/nvb/dev/officemanagement/controller/DocumentController.java
+++ src/main/java/nvb/dev/officemanagement/controller/DocumentController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/api/v1")
+@RequestMapping(path = "/api/v1/docManagement")
 @AllArgsConstructor
 public class DocumentController {
 
