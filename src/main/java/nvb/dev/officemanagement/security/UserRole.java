1e0aeb002e74bc84cdf07abef36b5de00d5b26af
Erfan Navab <erfannavab@gmail.com>
2024-02-03 20:24:32 +0330

    added user roles

---
+++ src/main/java/nvb/dev/officemanagement/security/UserRole.java
@@ -0,0 +1,30 @@
+package nvb.dev.officemanagement.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static nvb.dev.officemanagement.security.UserPermission.*;
+
+@AllArgsConstructor
+@Getter
+public enum UserRole {
+
+    ADMIN(Set.of(ADMIN_WRITE, ADMIN_READ)),
+    USER(Set.of(USER_READ));
+
+    private final Set<UserPermission> userPermissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> grantedAuthorities = getUserPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return grantedAuthorities;
+    }
+
+}
