49a5956a1f927ec58c7377f1f335332ff1986ebd
Erfan Navab <erfannavab@gmail.com>
2024-02-03 20:25:04 +0330

    added new constant variables

--- src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
+++ src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
@@ -9,5 +9,9 @@ public class SecurityConstant {
     public static final String MANAGER_URL = "/api/v1/managerManagement/**";
     public static final String OFFICE_URL = "/api/v1/officeManagement/**";
     public static final String DOC_URL = "/api/v1/docManagement/**";
+    public static final String USER_URL = "/api/v1/userManagement/**";
+
+    public static final String BEARER = "Bearer ";
+    public static final String SECRET_KEY = "ac5d152799de4870d25ed854c35373c721bcb747246624791baae95e";
 
 }
