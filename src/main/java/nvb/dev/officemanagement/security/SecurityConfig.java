da28620b3407182c24cd9985ec3c80be43653bf8
Erfan Navab <erfannavab@gmail.com>
2024-02-04 15:39:27 +0330

    - added authentication manager bean - configured filter and userDetailsService

--- src/main/java/nvb/dev/officemanagement/security/SecurityConfig.java
+++ src/main/java/nvb/dev/officemanagement/security/SecurityConfig.java
@@ -1,19 +1,19 @@
 package nvb.dev.officemanagement.security;
 
 import lombok.AllArgsConstructor;
+import nvb.dev.officemanagement.security.jwt.JwtAuthenticationFilter;
+import nvb.dev.officemanagement.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static nvb.dev.officemanagement.constant.SecurityConstant.*;
 import static nvb.dev.officemanagement.security.UserPermission.DOC_WRITE;
@@ -25,7 +25,8 @@ import static nvb.dev.officemanagement.security.UserRole.USER;
 @AllArgsConstructor
 public class SecurityConfig {
 
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -67,20 +68,17 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
 
                 )
-                .httpBasic(Customizer.withDefaults());
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails erfan = User.builder()
-                .username("erfan")
-                .password(passwordEncoder.encode("password123"))
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(erfan);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
