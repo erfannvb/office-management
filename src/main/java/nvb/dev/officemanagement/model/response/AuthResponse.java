faa592c0bcf2ada0ae9bea5436c7c2759e47add9
Erfan Navab <erfannavab@gmail.com>
2024-02-04 15:38:21 +0330

    added auth response

---
+++ src/main/java/nvb/dev/officemanagement/model/response/AuthResponse.java
@@ -0,0 +1,10 @@
+package nvb.dev.officemanagement.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class AuthResponse {
+    private String token;
+}
