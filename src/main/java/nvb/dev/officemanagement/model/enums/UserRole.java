c5686084d66e397bb6b98e3082978595229e9b80
Erfan Navab <erfannavab@gmail.com>
2024-02-05 20:50:46 +0330

    added ADMIN and USER

--- src/main/java/nvb/dev/officemanagement/model/enums/UserRole.java
+++ src/main/java/nvb/dev/officemanagement/model/enums/UserRole.java
@@ -1,15 +1,8 @@
 package nvb.dev.officemanagement.model.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum UserRole {
 
-    ROLE_ADMIN("role_admin"),
-    ROLE_USER("role_user");
-
-    private final String role;
+    ADMIN,
+    USER
 
 }
