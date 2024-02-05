7eb002e60c8e6efdb771ccf2b964d0642fc9677d
Erfan Navab <erfannavab@gmail.com>
2024-02-05 18:51:02 +0330

    added user role to the mappings

--- src/main/java/nvb/dev/officemanagement/mapper/UserMapper.java
+++ src/main/java/nvb/dev/officemanagement/mapper/UserMapper.java
@@ -10,13 +10,19 @@ import org.mapstruct.MappingConstants;
 public interface UserMapper {
 
     @Mapping(source = "userEntity.id", target = "id")
+    @Mapping(source = "userEntity.firstName", target = "firstName")
+    @Mapping(source = "userEntity.lastName", target = "lastName")
     @Mapping(source = "userEntity.username", target = "username")
     @Mapping(source = "userEntity.password", target = "password")
+    @Mapping(source = "userEntity.userRole", target = "userRole")
     UserDto toUserDto(UserEntity userEntity);
 
     @Mapping(source = "userDto.id", target = "id")
+    @Mapping(source = "userDto.firstName", target = "firstName")
+    @Mapping(source = "userDto.lastName", target = "lastName")
     @Mapping(source = "userDto.username", target = "username")
     @Mapping(source = "userDto.password", target = "password")
+    @Mapping(source = "userDto.userRole", target = "userRole")
     UserEntity toUserEntity(UserDto userDto);
 
 }
