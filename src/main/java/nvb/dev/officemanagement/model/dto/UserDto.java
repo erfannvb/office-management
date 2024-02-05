a3ec1897e1b3519b968fe94bc05b4aa2e0fdec45
Erfan Navab <erfannavab@gmail.com>
2024-02-05 20:48:32 +0330

    - implemented UserDetails - added new attributes

--- src/main/java/nvb/dev/officemanagement/model/dto/UserDto.java
+++ src/main/java/nvb/dev/officemanagement/model/dto/UserDto.java
@@ -1,18 +1,33 @@
 package nvb.dev.officemanagement.model.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nvb.dev.officemanagement.model.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-public class UserDto {
+public class UserDto implements UserDetails {
 
     private Long id;
+
+    @NotEmpty(message = "firstName cannot be empty.")
+    private String firstName;
+
+    @NotEmpty(message = "lastName cannot be empty.")
+    private String lastName;
 
     @NotEmpty(message = "username cannot be empty.")
     private String username;
@@ -20,4 +35,41 @@ public class UserDto {
     @NotEmpty(message = "password cannot be empty.")
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/nvb/dev/officemanagement/model/entity/UserEntity.java
+++ src/main/java/nvb/dev/officemanagement/model/entity/UserEntity.java
@@ -6,6 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nvb.dev.officemanagement.model.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "tbl_user")
@@ -13,18 +20,59 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "firstName cannot be empty.")
+    private String firstName;
+
+    @NotEmpty(message = "lastName cannot be empty.")
+    private String lastName;
+
     @NotEmpty(message = "username cannot be empty.")
-    @Column(name = "username", nullable = false)
     private String username;
 
     @NotEmpty(message = "password cannot be empty.")
-    @Column(name = "password", nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
