e00eca0ae056c5c3d32e1346dff7bf1892c2dcb7
Erfan Navab <erfannavab@gmail.com>
2024-02-06 18:34:21 +0330

    annotated the class

--- src/main/java/nvb/dev/officemanagement/model/response/JwtAuthResponse.java
+++ src/main/java/nvb/dev/officemanagement/model/response/JwtAuthResponse.java
@@ -1,8 +1,14 @@
 package nvb.dev.officemanagement.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 public class JwtAuthResponse {
 
     private String token;
