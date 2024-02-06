14a60890ca8d1db21afabac0cc5066e028710c62
Erfan Navab <erfannavab@gmail.com>
2024-02-06 09:40:13 +0330

    deleted unwanted constants

--- src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
+++ src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
@@ -10,8 +10,4 @@ public class SecurityConstant {
     public static final String OFFICE_URL = "/api/v1/officeManagement/**";
     public static final String DOC_URL = "/api/v1/docManagement/**";
     public static final String USER_URL = "/api/v1/userManagement/**";
-
-    public static final String BEARER = "Bearer ";
-    public static final String SECRET_KEY = "ac4000fd899172276b1be1c6d0d62e12c02297a956a25e80f5ff46ffd86f6a9a";
-
 }
