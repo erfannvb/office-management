678a231313027410507da39cb1ecfa48cec67fc0
Erfan Navab <erfannavab@gmail.com>
2024-02-06 13:28:47 +0330

    added new constants

--- src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
+++ src/main/java/nvb/dev/officemanagement/constant/SecurityConstant.java
@@ -10,4 +10,11 @@ public class SecurityConstant {
     public static final String OFFICE_URL = "/api/v1/officeManagement/**";
     public static final String DOC_URL = "/api/v1/docManagement/**";
     public static final String USER_URL = "/api/v1/userManagement/**";
+
+    public static final int TOKEN_EXPIRATION_TIME = 1000 * 60 * 24;
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 604800000;
+
+    public static final String BEARER = "Bearer ";
+    public static final String SECRET_KEY = "3910ea4f4f83c856cb26a29b048cdec3fc6dc2bd034ea97c9583e8ce08fffb5f";
+
 }
