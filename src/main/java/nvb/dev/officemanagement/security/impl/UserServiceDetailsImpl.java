57748d01569ea679ad9063a722d5bb3e96b0548f
Erfan Navab <erfannavab@gmail.com>
2024-02-06 17:27:05 +0330

    implemented user details service impl which uses loadByUsername method

---
+++ src/main/java/nvb/dev/officemanagement/security/impl/UserServiceDetailsImpl.java
@@ -0,0 +1,23 @@
+package nvb.dev.officemanagement.security.impl;
+
+import lombok.RequiredArgsConstructor;
+import nvb.dev.officemanagement.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceDetailsImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format("Username %s not found.", username)));
+    }
+
+}
