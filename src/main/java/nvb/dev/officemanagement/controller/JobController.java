30575c49250979033c94e44b330ef6b7ccbf28a7
Erfan Navab <erfannavab@gmail.com>
2024-02-07 23:57:34 +0330

    implemented job controller

---
+++ src/main/java/nvb/dev/officemanagement/controller/JobController.java
@@ -0,0 +1,42 @@
+package nvb.dev.officemanagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@RequiredArgsConstructor
+public class JobController {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @PostMapping(path = "/job")
+    public ResponseEntity<HttpStatus> launchJob() {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobInstanceAlreadyCompleteException | JobExecutionAlreadyRunningException |
+                 JobParametersInvalidException | JobRestartException e) {
+            e.getStackTrace();
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
