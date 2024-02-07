7d3daee2290b8e4b0b73359d4a6b7376d4f8203f
Erfan Navab <erfannavab@gmail.com>
2024-02-08 00:07:58 +0330

    - injected user service - invoking launchJob from user service

--- src/main/java/nvb/dev/officemanagement/controller/JobController.java
+++ src/main/java/nvb/dev/officemanagement/controller/JobController.java
@@ -1,11 +1,8 @@
 package nvb.dev.officemanagement.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import nvb.dev.officemanagement.service.UserService;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -20,22 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class JobController {
 
-    private final JobLauncher jobLauncher;
-    private final Job job;
+    private final UserService userService;
 
     @PostMapping(path = "/job")
-    public ResponseEntity<HttpStatus> launchJob() {
-
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
-
-        try {
-            jobLauncher.run(job, jobParameters);
-        } catch (JobInstanceAlreadyCompleteException | JobExecutionAlreadyRunningException |
-                 JobParametersInvalidException | JobRestartException e) {
-            e.getStackTrace();
-        }
-
+    public ResponseEntity<HttpStatus> launchJob() throws JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        userService.launchJob();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
