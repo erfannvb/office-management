b17a9a632c969bd250563bc0d0c02e679cc4da97
Erfan Navab <erfannavab@gmail.com>
2024-02-08 00:07:10 +0330

    - injected JobLauncher and Job - implemented launchJob method

--- src/main/java/nvb/dev/officemanagement/service/UserService.java
+++ src/main/java/nvb/dev/officemanagement/service/UserService.java
@@ -1,6 +1,10 @@
 package nvb.dev.officemanagement.service;
 
 import nvb.dev.officemanagement.model.entity.UserEntity;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +24,7 @@ public interface UserService {
     void deleteUser(long userId);
 
     boolean isExists(long userId);
+
+    void launchJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException;
 
 }
--- src/main/java/nvb/dev/officemanagement/service/impl/UserServiceImpl.java
+++ src/main/java/nvb/dev/officemanagement/service/impl/UserServiceImpl.java
@@ -6,6 +6,13 @@ import nvb.dev.officemanagement.exception.NoDataFoundException;
 import nvb.dev.officemanagement.model.entity.UserEntity;
 import nvb.dev.officemanagement.repository.UserRepository;
 import nvb.dev.officemanagement.service.UserService;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +25,8 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JobLauncher jobLauncher;
+    private final Job job;
 
     @Override
     public UserEntity createUser(UserEntity user) {
@@ -77,6 +86,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean isExists(long userId) {
         return userRepository.existsById(userId);
+    }
+
+    @Override
+    public void launchJob() throws JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        jobLauncher.run(job, new JobParametersBuilder().addLong("startAt",
+                System.currentTimeMillis()).toJobParameters());
     }
 
 }
