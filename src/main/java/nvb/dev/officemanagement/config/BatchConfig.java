aab36ab0dae188c29bd79ff12d5c30335bc2cdc6
Erfan Navab <erfannavab@gmail.com>
2024-02-07 23:56:33 +0330

    configured the batch

---
+++ src/main/java/nvb/dev/officemanagement/config/BatchConfig.java
@@ -0,0 +1,87 @@
+package nvb.dev.officemanagement.config;
+
+import lombok.RequiredArgsConstructor;
+import nvb.dev.officemanagement.model.entity.UserEntity;
+import nvb.dev.officemanagement.repository.UserRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public FlatFileItemReader<UserEntity> reader() {
+        FlatFileItemReader<UserEntity> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/data/user_data.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    private LineMapper<UserEntity> lineMapper() {
+        DefaultLineMapper<UserEntity> defaultLineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "username", "password", "userRole");
+
+        BeanWrapperFieldSetMapper<UserEntity> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(UserEntity.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return defaultLineMapper;
+    }
+
+    @Bean
+    public UserProcessor processor() {
+        return new UserProcessor();
+    }
+
+    @Bean
+    public RepositoryItemWriter<UserEntity> writer() {
+        RepositoryItemWriter<UserEntity> itemWriter = new RepositoryItemWriter<>();
+        itemWriter.setRepository(userRepository);
+        itemWriter.setMethodName("save");
+        return itemWriter;
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("csv-step", jobRepository)
+                .<UserEntity, UserEntity>chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job runJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("csv-job", jobRepository)
+                .flow(step(jobRepository, transactionManager))
+                .end().build();
+    }
+
+}
