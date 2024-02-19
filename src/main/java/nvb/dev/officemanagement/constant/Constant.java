5d14b4d2282d38f5013a568becc843a13298ce1f
erfannvb <erfannavab@gmail.com>
2024-02-20 00:08:02 +0330

    - generated a private constructor - added constant variables

--- src/main/java/nvb/dev/officemanagement/constant/Constant.java
+++ src/main/java/nvb/dev/officemanagement/constant/Constant.java
@@ -1,4 +1,11 @@
 package nvb.dev.officemanagement.constant;
 
 public class Constant {
+
+    private Constant() {
+    }
+
+    public static final String X_SYSTEM_NAME = "X-SystemName";
+    public static final String X_SYSTEM_PASSWORD = "X-SystemPassword";
+
 }
